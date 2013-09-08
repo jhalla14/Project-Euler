ffca7b8fc25232f19272630cef0b1e0ff249fa19
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-09-08 16:20:56 -0500

    begin problem 3

---
+++ src/Problem3.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joshua Hall
+ * Date: 9/8/13
+ * Time: 4:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Problem3 {
+
+    public static Double solve(){
+        Double answer = 0.0;
+
+
+        return answer;
+    }
+}
