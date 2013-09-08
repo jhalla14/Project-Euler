d71b555a22fa863846a52e6e1d4fa67c0279b0b8
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-09-07 21:50:47 -0500

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joshua Hall
+ * Date: 9/7/13
+ * Time: 9:45 PM
+ * To change this template use File | Settings | File Templates.
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+public class Main {
+
+    public static void main(String [ ] args)
+    {
+       System.out.print("Hello World");
+    }
+}
