5f8c87f4c6e7eba66bc9f12c20bee85c9df603f5
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-23 23:26:29 -0600

    palindrome checking method functional

--- src/Problem4.java
+++ src/Problem4.java
@@ -15,23 +15,55 @@ public class Problem4 {
 
     public static Double solve(){
 
-        System.out.println(isPalindrome(9009));
+        System.out.println(isPalindrome(90119));
 
         return 0.0;
     }
 
     private static boolean isPalindrome(Integer number){
 
+        boolean palindrome = false;
         String stringNumber = number.toString();
         char[] numberArray = stringNumber.toCharArray();
 
-        for (int i=0; i<numberArray.length;i++){
-           System.out.println(numberArray[i]);
+        if (numberArray != null){
+            char firstInt = numberArray[0];
+            char secondInt = numberArray[1];
+            char thirdInt = numberArray[2];
+            char fourthInt = numberArray[3];
+            char fifthInt = numberArray[4];
 
+            //check if the number is even or odd
+            if (numberArray.length % 2 == 0)        {
+                //then it must be of length 6
+                char sixthInt = numberArray[5];
+                if (firstInt == sixthInt && secondInt == fifthInt && thirdInt == fourthInt){
+                    palindrome = true;
+                }
+
+            } else {
+                //then it must be of length 5
+
+                if (firstInt == fifthInt && secondInt == fourthInt){
+                    palindrome = true;
+                }
+            }
+
+
+            for (int i=0; i<numberArray.length;i++){
+                System.out.println(numberArray[i]);
+
+            }
+
+            System.out.println(numberArray);
+        } else {
+            System.err.println("Array is null");
         }
 
-        System.out.println(numberArray);
 
-        return true;
+
+
+
+        return palindrome;
     }
 }
