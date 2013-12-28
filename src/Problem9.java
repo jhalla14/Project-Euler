feebbc110e045763ac05982ae38c608c9c1a1238
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-28 16:03:11 -0600

    started problem 9

---
+++ src/Problem9.java
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joshua Hall
+ * Date: 12/28/13
+ * Time: 4:01 PM
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+    a2 + b2 = c2
+    For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc.
+ */
+public class Problem9 {
+
+    public static Integer solve(){
+
+
+        return 0;
+    }
+}
