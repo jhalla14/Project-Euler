612185be064cf94f142cc701de500153fcccf3ff
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-28 17:56:07 -0600

    started working on problem 10

---
+++ src/Problem10.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joshua Hall
+ * Date: 12/28/13
+ * Time: 5:43 PM
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+public class Problem10 {
+
+    private final static Integer TWO_MILLION = 2000000;
+
+    public static Integer solve(){
+
+
+
+        return 0;
+    }
+}
